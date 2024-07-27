1412798e50d0d41f6774ec52878f6caba8872d06
Kopy <kylerkopacz@gmail.com>
2024-07-27 00:30:51 -0400

    more installation updates

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/zsh
-# remove the current setup
+
+# Install Oh-My-Zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Copy over the zsh configuration
+
+# Install Homebrew if on Mac
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install all of the homebrew stuff
+#xargs brew install < leaves
 
 # move everything one at a time
 

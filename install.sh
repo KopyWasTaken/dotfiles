23b8f04ce0b3fa4e50f2ab7530cd98ce67828687
Kopy <kylerkopacz@gmail.com>
2024-07-30 09:48:40 -0400

    udpdated install

--- install.sh
+++ install.sh
@@ -3,23 +3,14 @@
 # Install Oh-My-Zsh
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Copy over the zsh configuration
-cp .zshrc $HOME/
-
-# Source the Zsh configuration
-# source $HOME/.zshrc
-
 # Install Homebrew if on Mac
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install all of the homebrew stuff
 #xargs brew install < leaves
 
-# make the config directory if it doesn't exist yet
-mkdir -p $HOME/.config
-
-# move everything into the config 
-cp -r ./.config/* $HOME/.config
-
 # finally, see if you can install sdk man
 curl -s "https://get.sdkman.io" | bash
+
+# remind to install the nerd font
+echo "You're going to have to install the nerd font!"

abb0574ed103159fe2258714254711f5d4ecc987
KopyWasTaken <kylerkopacz@gmail.com>
2024-07-31 12:51:48 -0400

    tmux configuration

--- install.sh
+++ install.sh
@@ -9,8 +9,11 @@
 # install all of the homebrew stuff
 #xargs brew install < leaves
 
-# finally, see if you can install sdk man
+# see if you can install sdk man
 curl -s "https://get.sdkman.io" | bash
+
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # remind to install the nerd font
 echo "You're going to have to install the nerd font!"

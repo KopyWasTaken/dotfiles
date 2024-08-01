a5096dd4c942e73ac7637afdda93d0c09e956be7
kkopacz <kkopacz@chewy.com>
2024-08-01 14:40:09 -0400

    show hidden files

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,21 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required. Adds icons and what not
       "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = true,
+            hide_by_name = {
+              ".git",
+              ".gitignore",
+              ".DS_Store",
+            }
+          }
+        }
+      })
+    end
   }
 }

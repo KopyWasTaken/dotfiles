71592b6ec2f6d388e6165dd62ec385c827f3b2e5
kkopacz <kkopacz@chewy.com>
2024-08-06 18:55:39 -0400

    adding lombok functionality

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -33,6 +33,10 @@ return {
         capabilities = capabilities,
       })
       lspconfig.jdtls.setup({
+        cmd = {
+          "jdtls",
+          "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand "$MASON/share/jdtls/lombok.jar"),
+        },
         capabilities = capabilities,
       })
 

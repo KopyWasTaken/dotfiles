76eea8f320718ae0f1f2d5531d9b4f142eba012d
kkopacz <kkopacz@chewy.com>
2024-08-20 15:51:17 -0400

    basic updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,17 +3,17 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set shortcut to go back to normal mode from insert
-vim.keymap.set('i', 'jk', '<esc>', { desc = 'Mapping to get back to normal mode from insert'})
+vim.keymap.set('i', 'jk', '<esc>', { desc = 'Mapping to get back to normal mode from insert' })
 
 -- The other things from my old vim config
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<CR>', { desc = 'Remove search highlighting by pressing escape'})
+vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<CR>', { desc = 'Remove search highlighting by pressing escape' })
 
 -- Use line numbers
-vim.opt.number = true 
+vim.opt.number = true
 
 -- Keybind to reload the init.vim configs
-vim.keymap.set('n', '<Leader>r', '<cmd>source $HOME/.config/nvim/init.lua<CR>', { desc = 'Reload the nvim config'})
+vim.keymap.set('n', '<Leader>r', '<cmd>source $HOME/.config/nvim/init.lua<CR>', { desc = 'Reload the nvim config' })
 
 -- Case-insensitive searching unless \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
@@ -34,8 +34,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Tabbing stuff
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
-vim.opt.softtabstop=2
-vim.opt.shiftwidth=2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Wrapping stuff
+vim.opt.wrap = false
 
 -- Pull run the file in /lua/config/lazy.lua
 require("config.lazy")

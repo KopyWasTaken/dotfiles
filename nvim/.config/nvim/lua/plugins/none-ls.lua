0e210a9f8c1cc40effe25e6734294756c9a5ae4e
KopyWasTaken <kylerkopacz@gmail.com>
2024-09-27 10:43:55 -0400

    linux changes?

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		-- none-ls replaces the old deprecated null-ls, which wraps CLI tools in an LSP api, which is dope
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
+  {
+    -- none-ls replaces the old deprecated null-ls, which wraps CLI tools in an LSP api, which is dope
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua, -- formatter for lua
-					null_ls.builtins.formatting.clang_format, -- formatter for C / C++
-					null_ls.builtins.formatting.prettier, -- formatter for javascript things etc.
-					-- null_ls.builtins.formatting.pyright, -- formatter for python
-					null_ls.builtins.formatting.phpcsfixer, -- formatter for php
-					null_ls.builtins.formatting.prettier, -- formatter for web dev
-				},
-			})
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,  -- formatter for lua
+          null_ls.builtins.formatting.clang_format, -- formatter for C / C++
+          null_ls.builtins.formatting.prettier, -- formatter for javascript things etc.
+          null_ls.builtins.formatting.phpcsfixer, -- formatter for php
+          null_ls.builtins.formatting.prettier, -- formatter for web dev
+          null_ls.builtins.formatting.black, -- formatter for python 
+        },
+      })
 
-			-- Keymaps, with explanations
-			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {}) -- Use this keybind to format the file you're working with. Formatters can be installed through Mason
-		end,
-	},
+      -- Keymaps, with explanations
+      vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {}) -- Use this keybind to format the file you're working with. Formatters can be installed through Mason
+    end,
+  },
 }

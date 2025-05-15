efce847fb920775625c17fac8c13da7a3eb40302
KopyWasTaken <kylerkopacz@gmail.com>
2025-05-15 14:45:58 -0400

    backup script, it's beautiful

---
+++ dotfiles/.config/hypr/scripts/set_primary_monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Sets the primary monitor to the ultrawide
+# This is used for games launching through steam on the correct monitor
+# This should be ran on startup
+xrandr --output DP-2 --primary
---
+++ dotfiles/.config/scripts/backup_config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Sync the dotfiles
+rsync -aPhr --delete --filter="merge backup_config.filter" ~/.config/. ~/arch-bootstrap/dotfiles/.config/
+
+echo "==================== SYNCING FILES ===================="
+# Also sync the config files that live in the home directory
+rsync -aPh --include='.bashrc_custom' --include='.vimrc' --exclude='/**' ~/ ~/arch-bootstrap/dotfiles/

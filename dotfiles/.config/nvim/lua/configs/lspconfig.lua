598f9518b5e7a52b6e3d764fe72719946ecb4758
KopyWasTaken <kylerkopacz@gmail.com>
2025-07-03 08:32:47 -0400

    snapshot before going back to the dark side

--- dotfiles/.config/nvim/lua/configs/lspconfig.lua
+++ dotfiles/.config/nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "rust_analyzer", "bashls" }
+local servers = { "clangd", "rust_analyzer", "bashls", "gopls" }
 vim.lsp.enable(servers)

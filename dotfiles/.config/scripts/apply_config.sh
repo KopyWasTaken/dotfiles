9a534b6480355fd85044aee5cfe6c034158f791a
KopyWasTaken <kylerkopacz@gmail.com>
2025-11-22 12:44:14 -0500

    making this repo the config source of truth

--- dotfiles/.config/scripts/apply_config.sh
+++ dotfiles/.config/scripts/apply_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "==================== Applying Config Folders ===================="
-rsync -aPhr --delete --filter="merge $HOME/.config/scripts/config.filter" ~/dotfiles/dotfiles/.config/ ~/.config/. 
+rsync -aPhr --delete --filter="merge $HOME/dotfiles/dotfiles/.config/scripts/config.filter" ~/dotfiles/dotfiles/.config/ ~/.config/. 
 
 echo "==================== Applying Loose Files ===================="
 rsync -aPh --include='.bashrc_custom' --include='.vimrc' --include='.tmux.conf' --exclude='/**' ~/dotfiles/dotfiles/ ~/
--- dotfiles/.config/scripts/backup_config.sh
+++ dotfiles/.config/scripts/backup_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "==================== Syncing Config Folders ===================="
-rsync -aPhr --delete --filter="merge $HOME/.config/scripts/config.filter" ~/.config/. ~/dotfiles/dotfiles/.config/
+rsync -aPhr --delete --filter="merge $HOME/dotfiles/dotfiles/.config/scripts/config.filter" ~/.config/. ~/dotfiles/dotfiles/.config/
 
 echo "==================== Syncing Loose Files ===================="
 rsync -aPh --include='.bashrc_custom' --include='.vimrc' --include='.tmux.conf' --exclude='/**' ~/ ~/dotfiles/dotfiles/
